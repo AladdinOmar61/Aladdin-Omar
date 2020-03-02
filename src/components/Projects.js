2521e9ae1c9511773813b9d813b22abd7f2d3e1f
AladdinOmar61 <Aladdin.Omar61@gmail.com>
2020-03-02 14:19:46 -0500

    worked on more media queries

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,7 +5,7 @@ function Projects() {
     <div id="projects">
       <h2 className="project-header">Projects</h2>
       <ul className="projects">
-        <li>
+        <li className="single-project">
           <a
             href="http://bet-ch.surge.sh/"
             target="_blank"
@@ -17,9 +17,38 @@ function Projects() {
               alt="betch"
             />
           </a>
-          <p className="project-desc"><span className="betch">BET-CH:</span> the high-low betting card game! Guess if the next card drawn will be higher, lower, or the same as the current one on the screen. <span>bet-cha</span> can't beat it first try!</p>
+          <div className="desc-w-img">
+            <p className="project-desc">
+              <span className="project-name">BET-CH:</span> the high-low betting
+              card game! Guess if the next card drawn will be higher, lower, or
+              the same as the current one on the screen. <span>bet-cha</span>{" "}
+              can't beat it first try!
+            </p>
+            <div className="img-flex">
+              <img
+                className="lang-img"
+                src="https://image.flaticon.com/icons/svg/1216/1216733.svg"
+                alt="html"
+              />
+              <img
+                className="lang-img"
+                src="https://cdn4.iconfinder.com/data/icons/iconsimple-programming/512/css-512.png"
+                alt="css"
+              />
+              <img
+                className="lang-img"
+                src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/187_Js_logo_logos-512.png"
+                alt="javascript"
+              />
+              <img
+                className="lang-img"
+                src="https://cdn.iconscout.com/icon/premium/png-256-thumb/api-development-3-805959.png"
+                alt="API"
+              />
+            </div>
+          </div>
         </li>
-        <li>
+        <li className="single-project">
           <a
             href="http://ricpic-and-mortyschmorty.surge.sh/"
             target="_blank"
@@ -31,9 +60,35 @@ function Projects() {
               alt="rick and morty"
             />
           </a>
-          <p className="project-desc"><span>Ricpic & Morty Schmorty: </span>the Rick and Morty based social media app that lets you learn about the characters from the Adult Swim Series <span>Rick and Morty.</span> Look through the list of characters on the RicPics page then search the character by ID number on the search link for more details!</p>
+          <div className="desc-w-img">
+            <p className="project-desc">
+              <span className="project-name">Ricpic & Morty Schmorty: </span>the
+              Rick and Morty based social media app that lets you learn about
+              the characters from the Adult Swim Series{" "}
+              <span>Rick and Morty.</span> Look through the list of characters
+              on the RicPics page then search the character by ID number on the
+              search link for more details!
+            </p>
+            <div className="img-flex">
+              <img
+                className="lang-img"
+                src="https://cdn4.iconfinder.com/data/icons/iconsimple-programming/512/css-512.png"
+                alt="css"
+              />
+              <img
+                className="lang-img"
+                src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"
+                alt="react"
+              />
+              <img
+                className="lang-img"
+                src="https://cdn.iconscout.com/icon/premium/png-256-thumb/api-development-3-805959.png"
+                alt="API"
+              />
+            </div>
+          </div>
         </li>
-        <li>
+        <li className="single-project">
           <a
             href="http://cheetohphoto.surge.sh/"
             target="_blank"
@@ -45,9 +100,33 @@ function Projects() {
               alt="cheetoh"
             />
           </a>
-          <p className="project-desc"><span>Cheetoh Photo:</span> the cheetah based social media app! Log in and create, edit or delete your posts as well as comment on others, or just check out what other users have sent if you do not feel like loggin in.</p>
+          <div className="desc-w-img">
+            <p className="project-desc">
+              <span className="project-name">Cheetoh Photo:</span> the cheetah
+              based social media app! Log in and create, edit or delete your
+              posts as well as comment on others, or just check out what other
+              users have sent if you do not feel like loggin in.
+            </p>
+            <div className="img-flex">
+              <img
+                className="lang-img"
+                src="https://cdn4.iconfinder.com/data/icons/iconsimple-programming/512/css-512.png"
+                alt="css"
+              />
+              <img
+                className="lang-img"
+                src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"
+                alt="react"
+              />
+              <img
+                className="lang-img"
+                src="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/359/full/expressjslogo.png"
+                alt="express"
+              />
+            </div>
+          </div>
         </li>
-        <li>
+        <li className="single-project">
           <a
             href="http://peterpan.surge.sh/"
             target="_blank"
@@ -59,9 +138,34 @@ function Projects() {
               alt="peter pan donuts"
             />
           </a>
-          <p className="project-desc"><span>Peter Pan Donuts:</span> Special collaboration project with UX design team to redesign Peter Pan's Donut and Patry Shop's original website. Original site: <a href="http://peterpandonuts.com/" target="_blank" rel="noopener noreferrer"><span>http://peterpandonuts.com/</span></a></p>
+          <div className="desc-w-img">
+            <p className="project-desc">
+              <span className="project-name">Peter Pan Donuts:</span> Special
+              collaboration project with UX design team to redesign Peter Pan's
+              Donut and Patry Shop's original website. Original site can be found
+              <a className="old-peter-pan"
+                href="http://peterpandonuts.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span> here</span>
+              </a>
+            </p>
+            <div className="img-flex">
+              <img
+                className="lang-img"
+                src="https://cdn4.iconfinder.com/data/icons/iconsimple-programming/512/css-512.png"
+                alt="css"
+              />
+              <img
+                className="lang-img"
+                src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"
+                alt="react"
+              />
+            </div>
+          </div>
         </li>
-        <li className="project-flex">
+        <li className="single-project">
           <a
             href="http://smash-bros-deploy.s3-website-us-east-1.amazonaws.com/"
             rel="noopener noreferrer"
@@ -73,7 +177,39 @@ function Projects() {
               alt="super smash bros infinity"
             />
           </a>
-          <p className="project-desc"><span>Super Smash Brothers Infinity:</span> The Super Smash Bros. Ultimate fan site where you can create your own fun little fighter image with a set of four moves! Use the characters set in the original roster of the game as inspiration if you can't think of anything.</p>
+          <div className="desc-w-img">
+            <p className="project-desc">
+              <span className="project-name">
+                Super Smash Brothers Infinity:
+              </span>{" "}
+              The Super Smash Bros. Ultimate fan site where you can create your
+              own fun little fighter image with a set of four moves! Use the
+              characters set in the original roster of the game as inspiration
+              if you can't think of anything.
+            </p>
+            <div className="img-flex">
+              <img
+                className="lang-img"
+                src="https://cdn4.iconfinder.com/data/icons/iconsimple-programming/512/css-512.png"
+                alt="css"
+              />
+              <img
+                className="lang-img"
+                src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"
+                alt="react"
+              />
+              <img
+                className="lang-img"
+                src="https://res.cloudinary.com/practicaldev/image/fetch/c_scale,fl_progressive,q_auto,w_375/f_auto/https://dev-to-uploads.s3.amazonaws.com/uploads/badge/badge_image/32/rails-badge.png"
+                alt="ruby on rails"
+              />
+              <img
+                className="lang-img"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/993px-Postgresql_elephant.svg.png"
+                alt="postgresql"
+              />
+            </div>
+          </div>
         </li>
       </ul>
     </div>
