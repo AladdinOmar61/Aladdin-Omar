de55e0707cc4e5c79571ab82f46f424744ecc881
AladdinOmar61 <aladdin.omar@twgtech.com>
2020-10-16 15:38:02 -0400

    finished skills section with hover effects

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,25 +1,84 @@
-import React from 'react';
+import React from "react";
 
 function Skills() {
-    return (
-        <div className="skills">
-            <h2 className="skills__header margin-top-small">Skills</h2>
-            <div className="skills-grid margin-top-medium">
-            <img className="skills-img" src={"html5 (1).png"} alt="html"/>
-            <img className="skills-img" src={"css-3.png"} alt="css"/>
-            <img className="skills-img" src={"javascript.png"} alt="javascript"/>
-            <img className="skills-img" src={"react.png"} alt="react"/>
-            <img className="skills-img" src={"nodejs.png"} alt="node"/>
-            <img className="skills-img" src={"sequelize.png"} alt="sequelize"/>
-            <img className="skills-img" src={"sql-file-format-symbol.png"} alt="sql"/>
-            <img className="skills-img" src={"express.png"} alt="express"/>
-            <img className="skills-img" src={"python.png"} alt="python"/>
-            <img className="skills-img" src={"ruby.png"} alt="ruby"/>
-            <img className="skills-img" src={"ruby-on-rails.png"} alt="ruby on rails"/>
-            <img className="skills-img" src={"git.png"} alt="git"/>
-            <img className="skills-img" src={"github.png"} alt="github"/>
-            </div>
+  return (
+    <div className="skills">
+      <h2 className="skills__header margin-top-small">Skills</h2>
+      <div className="skills-grid margin-top-medium">
+        <div className="skills-div">
+          <p className="skills-div-name">HTML</p>
+          <img className="skills-div-img" src={"html5 (1).png"} alt="html" />
         </div>
-    )
+        <div className="skills-div">
+          <p className="skills-div-name">CSS</p>
+          <img className="skills-div-img" src={"css-3.png"} alt="css" />
+        </div>
+        <div className="skills-div">
+          <p className="skills-div-name">SASS</p>
+          <img className="skills-div-img" src={"sass.png"} alt="sass" />
+        </div>
+        <div className="skills-div">
+          <p className="skills-div-name">JAVASCRIPT</p>
+          <img
+            className="skills-div-img"
+            src={"javascript.png"}
+            alt="javascript"
+          />
+        </div>
+        <div className="skills-div">
+          <p className="skills-div-name">REACT</p>
+          <img className="skills-div-img" src={"react.png"} alt="react" />
+        </div>
+        <div className="skills-div">
+          <p className="skills-div-name">NODE</p>
+          <img className="skills-div-img" src={"nodejs.png"} alt="node" />
+        </div>
+        <div className="skills-div">
+          <p className="skills-div-name">SEQUELIZE</p>
+          <img
+            className="skills-div-img"
+            src={"sequelize.png"}
+            alt="sequelize"
+          />
+        </div>
+        <div className="skills-div">
+          <p className="skills-div-name">SQL</p>
+          <img
+            className="skills-div-img"
+            src={"sql-file-format-symbol.png"}
+            alt="sql"
+          />
+        </div>
+        <div className="skills-div">
+          <p className="skills-div-name">EXPRESS</p>
+          <img className="skills-div-img" src={"express.png"} alt="express" />
+        </div>
+        <div className="skills-div">
+          <p className="skills-div-name">PYTHON</p>
+          <img className="skills-div-img" src={"python.png"} alt="python" />
+        </div>
+        <div className="skills-div">
+          <p className="skills-div-name">RUBY</p>
+          <img className="skills-div-img" src={"ruby.png"} alt="ruby" />
+        </div>
+        <div className="skills-div">
+          <p className="skills-div-name">RUBY RAILS</p>
+          <img
+            className="skills-div-img"
+            src={"ruby-on-rails.png"}
+            alt="ruby on rails"
+          />
+        </div>
+        <div className="skills-div">
+          <p className="skills-div-name">GIT</p>
+          <img className="skills-div-img" src={"git.png"} alt="git" />
+        </div>
+        <div className="skills-div">
+          <p className="skills-div-name">GITHUB</p>
+          <img className="skills-div-img" src={"github.png"} alt="github" />
+        </div>
+      </div>
+    </div>
+  );
 }
-export default Skills
+export default Skills;
