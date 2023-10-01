f87412284d52ea7d1357d9bccefe0d9be86f3333
AladdinOmar61 <Aladdin.Omar61@gmail.com>
2023-10-01 00:29:04 -0400

    Cleaned Up Website
    Removed some things that no longer made sense to have

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,24 +6,8 @@ function Skills() {
       <h2 className="skills__header margin-top-small">Skills</h2>
       <div className="skills-grid margin-top-medium">
         <div className="skills-div">
-          <p className="skills-div-name">HTML</p>
-          <img className="skills-div-img" src={"html5 (1).png"} alt="html" />
-        </div>
-        <div className="skills-div">
-          <p className="skills-div-name">CSS</p>
-          <img className="skills-div-img" src={"css-3.png"} alt="css" />
-        </div>
-        <div className="skills-div">
           <p className="skills-div-name">SASS</p>
           <img className="skills-div-img" src={"sass.png"} alt="sass" />
-        </div>
-        <div className="skills-div">
-          <p className="skills-div-name">JAVASCRIPT</p>
-          <img
-            className="skills-div-img"
-            src={"javascript.png"}
-            alt="javascript"
-          />
         </div>
         <div className="skills-div">
           <p className="skills-div-name">REACT</p>
@@ -58,36 +42,12 @@ function Skills() {
           <img className="skills-div-img" src={"python.png"} alt="python" />
         </div>
         <div className="skills-div">
-          <p className="skills-div-name">RUBY</p>
-          <img className="skills-div-img" src={"ruby.png"} alt="ruby" />
-        </div>
-        <div className="skills-div">
-          <p className="skills-div-name">RUBY RAILS</p>
-          <img
-            className="skills-div-img"
-            src={"ruby-on-rails.png"}
-            alt="ruby on rails"
-          />
-        </div>
-        <div className="skills-div">
-          <p className="skills-div-name">GIT</p>
-          <img className="skills-div-img" src={"git.png"} alt="git" />
-        </div>
-        <div className="skills-div">
-          <p className="skills-div-name">GITHUB</p>
-          <img className="skills-div-img" src={"github.png"} alt="github" />
-        </div>
-        <div className="skills-div">
           <p className="skills-div-name">MONGO DB</p>
           <img className="skills-div-img" src={"mongodb.png"} alt="mongodb" />
         </div>
         <div className="skills-div">
           <p className="skills-div-name">SHOPIFY</p>
           <img className="skills-div-img" src={"shopify-logo.jpg"} alt="mongodb" />
-        </div>
-        <div className="skills-div">
-          <p className="skills-div-name">LIQUID.JS</p>
-          <img className="skills-div-img" src={"liquidJS.png"} alt="mongodb" />
         </div>
       </div>
     </div>
